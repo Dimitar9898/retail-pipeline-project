b8ca83a081f8811d39cf1cac34f284cbf0c291e2
Dimitar9898 <dimitar942@gmail.com>
2026-07-14 22:59:04 -0400

    star scheme

--- 02_load.py
+++ 02_load.py
@@ -17,3 +17,43 @@ df = pd.read_csv(r'C:\Users\dimit\Documents\retail_pipeline_project\cleaned_data
 
 print(df.shape)
 print(df.columns.tolist())
+
+#redistribute the data into dimension tables
+
+customers = df[['customer_id', 'customer_lat', 'customer_long']].drop_duplicates()
+
+for _, row in customers.iterrows():
+    cur.execute("""
+        INSERT INTO dim_customer (customer_id, customer_lat, customer_long)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (customer_id) DO NOTHING 
+    """, (row['customer_id'], row['customer_lat'], row['customer_long']))
+
+conn.commit()
+print("Loaded dim_customer")
+
+
+products = df[['product_name']].drop_duplicates()
+
+for _, row in products.iterrows():
+    cur.execute("""
+        INSERT INTO dim_product (product_name)
+        VALUES (%s)
+        ON CONFLICT (product_name) DO NOTHING
+    """, (row['product_name'],))
+
+conn.commit()
+print("Loaded dim_product")
+
+
+warehouses = df[['nearest_warehouse']].drop_duplicates()
+
+for _, row in warehouses.iterrows():
+    cur.execute("""
+        INSERT INTO dim_warehouse (warehouse_name)
+        VALUES (%s)
+        ON CONFLICT (warehouse_name) DO NOTHING
+    """, (row['nearest_warehouse'],))
+
+conn.commit()
+print("Loaded dim_warehouse")
