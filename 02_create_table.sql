94e57d22ab435fc8406b2a16ddbe8e7cc6ca8a84
Dimitar9898 <dimitar942@gmail.com>
2026-07-22 09:08:10 -0400

    Update retail database table creation scripts

--- 02_create_table.sql
+++ 02_create_table.sql
@@ -25,17 +25,12 @@ CREATE TABLE dim_date (
     season VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE fact_order_items (
-    order_item_key SERIAL PRIMARY KEY,
-    order_id VARCHAR(20) NOT NULL,
-
+CREATE TABLE fact_orders (
+    order_key SERIAL PRIMARY KEY,
+    order_id VARCHAR(20) NOT NULL UNIQUE,
     customer_key INT NOT NULL REFERENCES dim_customer(customer_key),
-    product_key INT NOT NULL REFERENCES dim_product(product_key),
     warehouse_key INT NOT NULL REFERENCES dim_warehouse(warehouse_key),
     date_key INT NOT NULL REFERENCES dim_date(date_key),
-
-    quantity INT NOT NULL,
-    order_price NUMERIC(10,2) NOT NULL,
     delivery_charges NUMERIC(10,2) NOT NULL,
     coupon_discount INT NOT NULL,
     order_total NUMERIC(10,2) NOT NULL,
@@ -43,4 +38,13 @@ CREATE TABLE fact_order_items (
     is_expedited_delivery BOOLEAN NOT NULL,
     is_happy_customer BOOLEAN NOT NULL,
     latest_customer_review TEXT
+);
+
+
+CREATE TABLE fact_order_items (
+    order_item_key SERIAL PRIMARY KEY,
+    order_key INT NOT NULL REFERENCES fact_orders(order_key),
+    product_key INT NOT NULL REFERENCES dim_product(product_key),
+    quantity INT NOT NULL,
+    order_price NUMERIC(10,2) NOT NULL
 );
